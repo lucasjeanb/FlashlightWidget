2c623c58ac32da98cb2f25c2857ac5300cdc9e23
Lucas <lucasjeanb@gmail.com>
2020-02-06 12:05:00 -0600

    startup flashlight widget

---
+++ app/src/main/java/com/example/flashlightwidget/FlashWidget.kt
@@ -0,0 +1,43 @@
+package com.example.flashlightwidget
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import com.example.vicky.clickablewidgetexample.MyPreference
+import java.util.*
+
+
+class ExampleAppWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        appWidgetIds.forEach { appWidgetId ->
+            // Create an Intent to launch ExampleActivity
+            val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+                .let { intent ->
+                    PendingIntent.getActivity(context, 0, intent, 0)
+                }
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.flash_widget
+            ).apply {
+                setOnClickPendingIntent(R.id.iv_sync, pendingIntent)
+            }
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
+
